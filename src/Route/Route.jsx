61332dc7fc2bd85aae4848505e7e2b56ca41c74d
mutanim24 <mutanim24@gmail.com>
2023-05-02 11:44:00 +0600

    project setup complete

---
+++ src/Route/Route.jsx
@@ -0,0 +1,11 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
